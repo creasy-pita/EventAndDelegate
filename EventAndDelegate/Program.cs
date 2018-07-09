741f215b6e2319f048af5f4be23c6eac5f7a542a
creasy-pita <hz_ljq@live.com>
2018-07-09 09:19:18 +0800

    Initial commit

---
+++ EventAndDelegate/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace EventAndDelegate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GreetPeople("en", "jesse");
+
+            Console.ReadLine();
+        }
+
+        static void GreetPeople(string lan, string name)
+        {
+            if (lan == "en")
+            {
+                EnlishGreeting(name);
+            }
+            else
+            {
+                ChineseGreeting(name);
+            }
+        }
+
+        static void EnlishGreeting(string name)
+        {
+            Console.Write(" morning ," + name);
+        }
+
+        static void ChineseGreeting(string name)
+        {
+            Console.Write(" 早上好 ," + name);
+        }
+    }
+}
