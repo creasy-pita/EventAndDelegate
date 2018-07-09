587287752353384286a25d96a03b51421cd072b6
creasy-pita <hz_ljq@live.com>
2018-07-09 10:05:59 +0800

    直接调用

--- EventAndDelegate/Program.cs
+++ EventAndDelegate/Program.cs
@@ -6,13 +6,9 @@ namespace EventAndDelegate
     {
         static void Main(string[] args)
         {
-            GreetingDelegate delegate1 = new GreetingDelegate(EnglishGreeting);// 创建委托类型变量的一种写法  new GreetingDelegate
-            GreetingDelegate delegate2 = new GreetingDelegate(ChineseGreeting);
-            //GreetingDelegate delegate1, delegate2;
-            //delegate1 = EnglishGreeting;//委托类型 和 EnglishGreeting 类型 方法定义上等价可以直接赋值；
-            //delegate2 = ChineseGreeting;
-            GreetPeople("creasypita", delegate1);
-            GreetPeople("卢俊强", delegate2);
+            GreetingDelegate delegate1 = ChineseGreeting;
+            delegate1 += EnglishGreeting;//多个参数链
+            delegate1("creasypita");//直接调用
 
             Console.ReadLine();
         }
--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # EventAndDelegate
+
+1   
+delegate void GreetingDelegate(string name)
+event  GreetingDelegate  MakeGreet
+相同点：
+	GreetingDelegate 是一个委托类型
+	MakeGreet event 也是一个GreetingDelegate类型的委托
+不同点
+	访问性上不同
+		public event  GreetingDelegate  MakeGreet;
+		类内部 可以直接访问
+		外部类 是一个私有方法private 
+			需要通过 -=,+=来使用
